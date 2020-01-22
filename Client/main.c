818c89ff01e73a1085d83a6139e3f6ac9e835fce
apetroaieclaudiu <apetroaie.claudiu@gmail.com>
2020-01-22 17:08:39 +0200

    Prototype implementation. Should be tested

--- Client/main.c
+++ Client/main.c
@@ -8,30 +8,23 @@
 #include <signal.h>
 
 #define MSG_SIZE            4096
-#define CLIENT_QUEUE_NAME   "Client"
 #define SERVER_QUEUE_NAME   "Server"
 // This handler will be called when the queue 
 // becomes non-empty.
 
 char buf[MSG_SIZE];              // A good-sized buffer
 mqd_t mqrecv, mqsend;    
-void handler (int sig_num) {
-    if (mq_receive (mqrecv, &buf[0], MSG_SIZE, NULL) != -1) {
-        printf("The token recieved is: %s", buf);
-    }
-    else if (errno != EAGAIN) { 
-      perror ("mq_receive()");
-      _exit (EXIT_FAILURE);
-    }
-}
 
 void main () {
-
-    struct mq_attr attr, old_attr;   // To store queue attributes
-    struct sigevent sigevent;        // For notification
+    char*   clientQueue = "Client";
+    int r = rand() % 10;
+    char    str[8];
+    sprintf(str, "%d", r);
+    strcat(clientQueue, str);
+    struct  mq_attr attr, old_attr;   // To store queue attributes
              // Message queue descriptors
     unsigned int prio;               // Priority 
-
+    
     // First we need to set up the attribute structure
     attr.mq_maxmsg = 300;
     attr.mq_msgsize = MSG_SIZE;
@@ -43,29 +36,21 @@ void main () {
     // Open a queue with the attribute structure
     mqsend = mq_open (SERVER_QUEUE_NAME, O_WRONLY, 
                     0664, &attr);
-    mqrecv = mq_open (CLIENT_QUEUE_NAME, O_RDONLY | O_CREAT, 
+    mqrecv = mq_open (clientQueue, O_RDONLY | O_CREAT, 
                     0664, &old_attr);
     // Get the attributes for server queue
     mq_getattr (mqsend, &attr);
 
-    // We want to be notified when something is there 
-    signal (SIGUSR1, handler);
-    sigevent.sigev_signo = SIGUSR1;
-
-    if (mq_notify (mqrecv, &sigevent) == -1) {
-    if (errno == EBUSY) 
-        printf (
-        "Another process has registered for notification.\n");
-    _exit (EXIT_FAILURE);
-    }
-
     while (1) {
         char *msg = "Token Request\n";
-        strcat(msg, CLIENT_QUEUE_NAME);
+        strcat(msg, clientQueue);
         int sleepTime = rand() % 10;
         sleep(sleepTime);
-        if (mq_send (mqsend, msg, strlen(msg), 0) == -1)
+        if (mq_send(mqsend, msg, strlen(msg), 0) == -1)
             perror ("mq_send()");
+
+        mq_receive(mqrecv, buf, MSG_SIZE, NULL);
+        printf("Received the token with number: %d\n", buf);
     }
 
   // Close all open message queue descriptors    
--- Server/main.c
+++ Server/main.c
@@ -8,27 +8,16 @@
 #include <signal.h>
 
 #define MSG_SIZE            4096
-#define CLIENT_QUEUE_NAME   "Client"
 #define SERVER_QUEUE_NAME   "Server"
 // This handler will be called when the queue 
 // becomes non-empty.
 
 char buf[MSG_SIZE];              // A good-sized buffer
 mqd_t mqrecv, mqsend;    
-void handler (int sig_num) {
-    if (mq_receive (mqrecv, &buf[0], MSG_SIZE, NULL) != -1) {
-        printf("The token recieved is: %s", buf);
-    }
-    else if (errno != EAGAIN) { 
-      perror ("mq_receive()");
-      _exit (EXIT_FAILURE);
-    }
-}
 
 void main () {
 
     struct mq_attr attr, old_attr;   // To store queue attributes
-    struct sigevent sigevent;        // For notification
              // Message queue descriptors
     unsigned int prio;               // Priority 
 
@@ -41,31 +30,37 @@ void main () {
     old_attr.mq_msgsize = MSG_SIZE;
     old_attr.mq_flags = 0;
     // Open a queue with the attribute structure
-    mqsend = mq_open (SERVER_QUEUE_NAME, O_WRONLY, 
+    mqrecv = mq_open (SERVER_QUEUE_NAME, O_RDONLY | O_CREAT, 
                     0664, &attr);
-    mqrecv = mq_open (CLIENT_QUEUE_NAME, O_RDONLY | O_CREAT, 
-                    0664, &old_attr);
     // Get the attributes for server queue
-    mq_getattr (mqsend, &attr);
-
-    // We want to be notified when something is there 
-    signal (SIGUSR1, handler);
-    sigevent.sigev_signo = SIGUSR1;
-
-    if (mq_notify (mqrecv, &sigevent) == -1) {
-    if (errno == EBUSY) 
-        printf (
-        "Another process has registered for notification.\n");
-    _exit (EXIT_FAILURE);
-    }
+    mq_getattr (mqrecv, &attr);
 
     while (1) {
-        char *msg = "Token Request\n";
-        strcat(msg, CLIENT_QUEUE_NAME);
-        int sleepTime = rand() % 10;
-        sleep(sleepTime);
-        if (mq_send (mqsend, msg, strlen(msg), 0) == -1)
+        char    rcvmsg[MSG_SIZE];
+        int     i = 0, j = 0;
+        char    clientQueue[10];
+        int     startWriting = 0;
+        int r = rand() % 10;
+        char    str[8];
+        sprintf(str, "%d", r);
+        mq_receive(mqrecv, rcvmsg, MSG_SIZE, NULL);
+        while(*(rcvmsg + i)) {
+            if (startWriting) {
+                clientQueue[j] = *(rcvmsg + i);
+                j++;
+            }
+            if (*(rcvmsg + i) == '\n') {
+                startWriting = 1;
+            }
+            i++;
+        }
+        clientQueue[j] = '\0';
+        mqsend = mq_open (clientQueue, O_WRONLY, 0664, &attr);
+    
+        if (mq_send(mqsend, str, strlen(str), 0) == -1)
             perror ("mq_send()");
+            
+        mq_close(mqsend);
     }
 
   // Close all open message queue descriptors    
